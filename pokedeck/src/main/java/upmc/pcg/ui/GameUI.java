68a70542b04066b36759dd60fc0e78b98adc728b
adminRAF <felana.ramarlina@hotmail.fr>
2017-11-06 13:59:34 +0100

    Methode du menu

--- pokedeck/src/main/java/upmc/pcg/ui/GameUI.java
+++ pokedeck/src/main/java/upmc/pcg/ui/GameUI.java
@@ -15,6 +15,7 @@
 package upmc.pcg.ui;
 
 import java.util.*;
+import upmc.pcg.Pokedeck;
 import upmc.pcg.game.Game;
 
 public class GameUI 
@@ -52,7 +53,45 @@ public class GameUI
           playersNames.add(playerName);  
           i++;
       }
-      
+  }
+  
+  /*pas encore fonctionnelle car non finie*/
+  public void menu(Pokedeck p)
+  {
+      System.out.println("-- Menu --");
+      System.out.println("1 - Display my collection\n 2 - Search specific card\n 3 - Quit");
+      int choice= this.console.nextInt();
+        
+      while(choice!=1 || choice !=2 || choice!=3)
+      {
+        switch(choice){
+            case 1:
+                p.displayCollection();
+            break;
+            case 2:
+                System.out.println("1 - Search card by name\n 2 - Search card by number\n 3 - Return");
+                choice = this.console.nextInt();
+                if(choice==1){
+                    this.console.nextLine();
+                    System.out.println("Card name :");
+                    String name = this.console.nextLine();
+                }else if(choice==2){
+                    System.out.println("Card number :");
+                    int number = this.console.nextInt();
+                }else if(choice==3){   
+                }
+                else{
+                    System.out.print("Error! Please make a choicie between 1-3");
+                }
+
+            break;
+            case 3:
+                System.out.print("En game");
+            ;
+            default:
+                System.out.print("Error! Please make a choicie between 1-3");
+        }
+      }
   }
   
   private void print_welcome_msg() 
